9965968e8e2bda51b58ac87fcf822ea5a3c91d05
DiOnEasy <d11062002d@gmail.com>
2024-02-05 20:21:40 +0200

    settings, header, sidebar adaptibility are done

--- src/components/SideBar/sideBar.jsx
+++ src/components/SideBar/sideBar.jsx
@@ -1,5 +1,6 @@
 import { Link, useLocation } from "react-router-dom";
 import s from "./sideBar.module.css";
+import { useEffect, useState } from "react";
 
 export const SideBar = () => {
   const links = [
@@ -12,28 +13,77 @@ export const SideBar = () => {
     { text: "Notifications", link: "notifications" },
   ];
 
-  const location = useLocation()
+  const location = useLocation();
+
+  const [sidebarOpened, setSidebarOpened] = useState(false);
+
+  const [isTablet, setIsTablet] = useState(null);
+  const handleResize = () => {
+    setIsTablet(window.innerWidth <= 1280);
+  };
+
+  useEffect(() => {
+    // Установите начальное значение при монтировании компонента
+    handleResize();
+
+    // Добавьте слушатель события изменения размера окна
+    window.addEventListener('resize', handleResize);
+
+    // Очистите слушатель события при размонтировании компонента
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
-    <div className={s.sidebar}>
-      <div className={s.logo}>
-        <img src="/img/logo-white.svg" alt="" />
-      </div>
-      <div className={s.links}>
-        {links.map((link, index) => {
-         const isActive = (`${link.link}`) == location.pathname.split('/')[1];
-          return (
-            <Link className={isActive ? s.activeLink : ''} key={index} to={`/${link.link}`}>
-              <img src={`/img/sidebar-icons/link-${index + 1}.svg`} alt="" />
-              {link.text}
-            </Link>
-          );
-        })}
-      </div>
-      <div className={s.profile}>
-        <div className={s.profile__img}><img src="" alt="" /></div>
-        <p className={s.profile__email}>mail@mail.com</p>
-        <button className={s.profile__button}><img src="/img/sidebar-icons/exit.svg" alt="" /><span>Exit</span></button>
+    <div className={s.sidebarWrapper}>
+      {isTablet && (
+        <div
+          onClick={() => setSidebarOpened(!sidebarOpened)}
+          className={sidebarOpened ? `${s.burger} ${s.burgerActive}` : s.burger}
+        >
+          {sidebarOpened ? (
+            <img src="/img/close.svg" alt="" />
+          ) : (
+            <img src="/img/burger.svg" alt="" />
+          )}
+        </div>
+      )}
+
+      <div
+        className={
+          !sidebarOpened ? ` ${s.sidebarClosed} ${s.sidebar}` : s.sidebar
+        }
+      >
+        <div className={s.logo}>
+          <img src="/img/logo-white.svg" alt="" />
+        </div>
+        <div className={s.links}>
+          {links.map((link, index) => {
+            const isActive = `${link.link}` == location.pathname.split("/")[1];
+            return (
+              <Link
+                onClick={() => setSidebarOpened(!sidebarOpened)}
+                className={isActive ? s.activeLink : ""}
+                key={index}
+                to={`/${link.link}`}
+              >
+                <img src={`/img/sidebar-icons/link-${index + 1}.svg`} alt="" />
+                {link.text}
+              </Link>
+            );
+          })}
+        </div>
+        <div className={s.profile}>
+          <div className={s.profile__img}>
+            <img src="" alt="" />
+          </div>
+          <p className={s.profile__email}>mail@mail.com</p>
+          <button className={s.profile__button}>
+            <img src="/img/sidebar-icons/exit.svg" alt="" />
+            <span>Exit</span>
+          </button>
+        </div>
       </div>
     </div>
   );
